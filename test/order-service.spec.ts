531836c4eb3ea73f38337431587e0c69be2e6317
cazzalles <ferreira.tavares.davi@gmail.com>
2024-07-14 19:39:26 -0300

    Princípio de testes

--- test/order-service.spec.ts
+++ test/order-service.spec.ts
@@ -1,23 +1,13 @@
-import { OrderService } from "src/order/order.service";
-import { PrismaService } from "src/database/prisma.service";
-import { ItemService } from "src/order/item/item.service"; 
-import { Order } from "src/order/dto/order.dto";
+import { PrismaService } from "../src/database/prisma.service";
+import { ItemService } from "../src/order/item/item.service";
+import { OrderService } from "../src/order/order.service";
 
-const prisma = new PrismaService();
-const itemService = new ItemService(prisma); 
-const order = new OrderService(prisma, itemService); 
-const orderBody = new Order(); 
 
-test('Order service', () => {
-    expect(order).toBe(OrderService); 
+test('OrderService criada', () => {
+    const prismaService = new PrismaService(); 
+    const itemService = new ItemService(prismaService); 
+    const orderService = new OrderService(prismaService, itemService); 
+
+    expect(typeof orderService).toBe("object");
+
 });
-
-test('Order service create', () => {
-    const create = async () =>{
-        await order.create(orderBody) 
-    }
-    
-    expect(create()).toBe(orderBody); 
-});
-
-// tá completamente embrionário
